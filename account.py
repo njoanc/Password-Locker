7ae215f7a12f0237139aa79bdd86fe60eb553b34
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-11 12:44:04 +0200

    testing our Account class

--- account.py
+++ account.py
@@ -11,8 +11,7 @@ class Account:
 
     account_list = []  # empty account list
 
-    def __init__(self, account_name, user_name, password, email):
+    def __init__(self, account_name, user_name, password):
         self.account_name = account_name
         self.user_name = user_name
         self.password = password
-        self.email = email
---
+++ account_test.py
@@ -0,0 +1,12 @@
+import unittest  # Importing the unittest module
+from account import Account  # Importing the account class
+
+
+class TestAccount(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the account class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
