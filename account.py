9e8cec087b7b79eed6d093ea6af7e4aae24da609
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-11 09:05:42 +0200

    Instance and class variables

--- account.py
+++ account.py
@@ -1,7 +1,6 @@
 
 class Account:
 
-
     """
 
 
@@ -9,3 +8,11 @@ class Account:
 
 
     """
+
+    account_list = []  # empty account list
+
+    def __init__(self, account_name, user_name, password, email):
+        self.account_name = account_name
+        self.user_name = user_name
+        self.password = password
+        self.email = email
